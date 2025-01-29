85f5f8817520060e3016077f7a9064d27736bb69
Julia <email96123@gmail.com>
2025-01-29 16:05:31 -0700

    add sample

--- Code/histogram.py
+++ Code/histogram.py
@@ -3,9 +3,8 @@ import re
 
 def get_source_text(filename):
     with open(filename, "r") as file:
-        content = file.read()
-        cleaned_file = re.sub('[^A-Za-z0-9]+', ' ', content)
-        return cleaned_file.lower()
+        content = file.read().lower()
+        return re.sub('[^A-Za-z0-9]+', ' ', content)
 
 
 def histogram(source_text):
@@ -24,10 +23,17 @@ def frequency(word, histogram):
     return histogram.get(word, 0)
 
 
+def sample(histogram):
+    sorted_list = sorted(histogram.items(), key=lambda x: x[1])
+    return sorted_list[-1][0]
+
+
 if __name__ == "__main__":
     source_text = get_source_text('./Code/data/sample.txt')
     hist = histogram(source_text)
 
-    print(hist)
-    print(unique_words(hist))
-    print(frequency("red", hist))
+    # print(hist)
+    # print(unique_words(hist))
+    # print(frequency("red", hist))
+    # print(sort_histogram(hist))
+    print(sample(hist))
