e25c5f22bf2531807bf77d892b3bf36b76ee0113
Julia <email96123@gmail.com>
2025-02-23 12:52:08 -0700

    get next token from weighted values

--- Code/utils/markov.py
+++ Code/utils/markov.py
@@ -23,7 +23,11 @@ class MarkovChain:
         path = [start_word]
         node = self.chain[start_word]
         for _ in range(len):
-            next_token = random.choice(list(node.keys()))
+            # https://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
+            options = list(node.keys())
+            next_token = random.choices(options, weights=node.values())[0]
+
             path.append(next_token)
             node = self.chain[next_token]
+
         return " ".join(path)
